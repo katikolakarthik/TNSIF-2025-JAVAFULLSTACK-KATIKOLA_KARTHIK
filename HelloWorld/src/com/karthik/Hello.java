20a8f255305c27ad4ab6f7550cec0e3172e688f3
karthik <katikolakarthik@gmail.com>
2025-09-13 11:15:38 +0530

    new commit java

---
+++ HelloWorld/src/com/karthik/Hello.java
@@ -0,0 +1,11 @@
+package com.karthik;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello karthikkatikola namaste");
+
+	}
+
+}
